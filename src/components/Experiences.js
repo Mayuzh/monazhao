2f8bab75c216a19f47323ef65f77c926d683f3ac
Mona Zhao <70651849+Mayuzh@users.noreply.github.com>
2024-01-29 16:50:04 -0800

    publi

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,6 +1,8 @@
 // components/Home.js
 import React from 'react';
 import { FaChalkboardTeacher } from "react-icons/fa";
+import { IoCodeWorkingSharp } from "react-icons/io5";
+import { MdComputer } from "react-icons/md";
 
 const Experiences = () => {
     // Add the content of the home section here
@@ -12,14 +14,32 @@ const Experiences = () => {
             </div>
 
             <div className='flex flex-col p-4 w-[70%] space-y-6 leading-relaxed'>
-                <div className='flex flex-col'>
+                <div className='flex flex-col mt-8 space-y-6'>
+                    <div className='normal flex flex-row'>
+                        <MdComputer size={28} className='mr-5 mt-3 text-pink-950' />
+                        <div className='flex flex-col'>
+                            <span className='text-[30px]'>Research</span>
+                            <span className='text-stone-500 text-base'>UC Santa Cruz</span>
+                            <span className='text-[21px]'>Undergraduate Research Intern</span>
+                            <span className='text-[21px]'>Graduate Student Researcher</span>
+                        </div>
+                    </div>
+                    <div className='normal flex flex-row'>
+                        <IoCodeWorkingSharp size={28} className='mr-5 mt-3 text-pink-950' />
+                        <div className='flex flex-col'>
+                            <span className='text-[30px]'>Software Developer</span>
+                            <span className='text-stone-500 text-base'>CITRIS and the Banatao Institute - CITRIS Workforce Innovation</span>
+                            <span className='text-[21px]'>RoboticsCats</span>
+                        </div>
+                    </div>
                     <div className='normal flex flex-row'>
                         <FaChalkboardTeacher size={28} className='mr-5 mt-3 text-pink-950' />
                         <div className='flex flex-col'>
-                            <span className='text-[30px]'>Tutor</span>
+                            <span className='text-[30px]'>Group Tutor and Reader</span>
                             <span className='text-stone-500 text-base'>UC Santa Cruz</span>
-                            <span className='text-[21px]'>CSE30, CSE101, CSE144</span>
-                           
+                            <span className='text-[21px]'>CSE 30 (Programming Abstractions: Python)</span>
+                            <span className='text-[21px]'>CSE 101 (Introduction to Data Structures and Algorithms)</span>
+                            <span className='text-[21px]'>CSE 144 (Applied Machine Learning: Deep Learning)</span>
                         </div>
                     </div>
 
--- src/components/Publications.js
+++ src/components/Publications.js
@@ -7,12 +7,20 @@ const Publications = () => {
     return (
         <div id='publications' className='title flex flex-row max-w-7xl mx-auto px-6'>
             <div className='flex flex-col w-[30%] py-8 pl-4'>
-               <div className='text-[50px]'>{title}</div>
+                <div className='text-[50px]'>{title}</div>
             </div>
 
             <div className='flex flex-col p-4 w-[70%] space-y-6 leading-relaxed'>
-               
-  
+                <div className='flex flex-col mt-8'>
+                    <span className='title-black text-[30px] hover:underline'>
+                        <a href="https://ieeexplore.ieee.org/document/10041994" target="_blank" rel="noopener noreferrer">
+                            RipViz: Finding Rip Currents by Learning Pathline Behavior
+                        </a>
+                    </span>
+                    <span className='normal text-[16px] pb-4'>We present a hybrid machine learning and flow analysis feature detection method, RipViz, to extract rip currents from stationary videos. Rip currents are dangerous strong currents that can drag beachgoers out to sea. Most people are either unaware of them or do not know what they look like. In some... </span>
+                    <span className='normal text-stone-500 text-[14px]'>Akila de Silva, Mona Zhao, Donald Stewart, Fahim Hasan, Gregory Dusek, James Davis, Alex Pang</span>
+                    <span className='normal text-stone-500 text-[14px]'>February 2023  - IEEE</span>
+                </div>
             </div>
         </div>
     );
